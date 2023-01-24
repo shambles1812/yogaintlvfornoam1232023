261e4aaa76876d94df1c0d947f28557c3cf6305b
shambles1812 <cjvillafuerte02@gmail.com>
2023-01-24 14:10:03 +0800

    django debug

--- backend/urls.py
+++ backend/urls.py
@@ -1,14 +1,16 @@
 # backend/urls.py
 
 from django.contrib import admin
-from django.urls import path, include                   # add this
+from django.urls import path, include, re_path               # add this
 from rest_framework import routers                      # add this
 from yoga import views                                  # add this
+from django.views.generic import TemplateView
 
 router = routers.DefaultRouter()                        # add this
 router.register(r'yoga', views.YogaView, 'yoga')       # add this
 
 urlpatterns = [
     path('admin/', admin.site.urls),         
-    path('api/', include(router.urls))                   # add this
+    path('api/', include(router.urls)),                   # add this
+    re_path('.*',TemplateView.as_view(template_name="index.html"))
 ]
