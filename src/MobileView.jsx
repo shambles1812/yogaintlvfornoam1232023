167f1ec2c97004dc4f523d620c3b252ba5fff669
shambles1812 <cjvillafuerte02@gmail.com>
2023-01-26 06:39:42 +0800

    test

--- src/MobileView.jsx
+++ src/MobileView.jsx
@@ -89,7 +89,7 @@ const MobileView = () => {
             
             <div className="h-1/3 bg-lavander ">
             <Swiper
-                initialSlide={23-chosenHour}
+                initialSlide={20-chosenHour}
                 slidesPerView={4.5}
                 spaceBetween={5}
                 className="hourSwiper"
