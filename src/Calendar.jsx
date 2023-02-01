df5021d7d63b7ed7df623453aa85afaff64f3dce
shambles1812 <cjvillafuerte02@gmail.com>
2023-02-02 07:58:18 +0800

    test

--- src/Calendar.jsx
+++ src/Calendar.jsx
@@ -133,7 +133,8 @@ const Calendar = ({setFetching,setMobileDate,chosenDate,setMobileHour,chosenHour
                       withCredentials:true
                     }).then(res => {
                         setSlideJson(res.data);
-                        
+                        console.log("I FOUND SOME DATA")
+                        console.log(res.data)
                         var my_array = res.data
                          my_array.sort(
                             (a,b) => {
